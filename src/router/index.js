189d9a5e8861fd61232eca1a0085cff5b92b64fa
bboodd <gkrgus9@gmail.com>
2024-08-30 13:51:48 +0900

    정해지지 않은 요청 핸들링

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,64 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import PageHome from '@/views/PageHome.vue'
-import BoardView from '@/views/board/BoardView.vue'
-import PostList from '@/components/board/PostList.vue'
-import PostDetail from '@/components/board/PostDetail.vue'
-import PostWrite from '@/components/board/PostWrite.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import PageHome from "@/views/PageHome.vue";
+import BoardView from "@/views/BoardView.vue";
+import PostList from "@/components/board/PostList.vue";
+import PostDetail from "@/components/board/PostDetail.vue";
+import PostWrite from "@/components/board/PostWrite.vue";
+import NotFoundPage from "@/views/NotFoundPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "PageHome",
+      component: PageHome,
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: () => import("../views/PageAbout.vue"),
+    },
+    {
+      path: "/board",
+      name: "board",
+      component: BoardView,
+      children: [
         {
-            path: '/',
-            name: 'PageHome',
-            component: PageHome
+          path: "",
+          name: "postList",
+          component: PostList,
         },
         {
-            path: '/about',
-            name: 'About',
-            component: () => import('../views/PageAbout.vue')
+          path: "write",
+          name: "postWrite",
+          component: PostWrite,
+          children: [
+            {
+              path: "",
+              name: "postSave",
+              component: PostWrite,
+            },
+            {
+              path: ":postId",
+              name: "postUpdate",
+              component: PostWrite,
+            },
+          ],
         },
         {
-            path: '/board',
-            name: 'board',
-            component: BoardView,
-            children: [
-                {
-                    path: '',
-                    name: 'postList',
-                    component: PostList
-                },
-                {
-                    path: 'write',
-                    name: 'postWrite',
-                    component: PostWrite
-                },
-                {
-                    path: ':postId',
-                    name: 'postDetail',
-                    component: PostDetail
-                }
-            ]
-        }
-    ]
-})
+          path: "detail/:postId",
+          name: "postDetail",
+          component: PostDetail,
+        },
+      ],
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notFoundPage",
+      component: NotFoundPage,
+    },
+  ],
+});
 
-export default router
+export default router;
